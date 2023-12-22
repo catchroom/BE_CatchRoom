4e3ca94bed086688bbb2fef3c66c810d59e76b16
Parkgeonmoo <pgw111111@naver.com>
2023-12-22 16:28:34 +0900

    Feat : CORS 기본 설정 세팅
    -CORS 기본 설정 세팅

--- src/main/java/com/example/catchroom_be/Controller/TestController.java
+++ src/main/java/com/example/catchroom_be/Controller/TestController.java
@@ -1,17 +1,15 @@
 package com.example.catchroom_be.Controller;
 
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/test")
+
 public class TestController {
 
-    @PostMapping("/do")
-    public String Test(@RequestParam String name) {
-        return "나는 " + name + "입니다.";
+    @GetMapping("/do")
+    public String Test() {
+        return "나는 박건우입니다.";
     }
 }
