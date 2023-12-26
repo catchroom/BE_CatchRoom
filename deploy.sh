6e951c97c641aef57b7b260afd4a8566ffa0e973
Parkgeonmoo <pgw111111@naver.com>
2023-12-26 09:54:51 +0900

    Refact : 빌드 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -27,5 +27,5 @@ else
 fi
 
 
-echo "> $JAR_PATH 배포 >> sudo -u ubuntu nohup sh -c "java -jar $JAR_PATH > $LOG_PATH" &"
-sudo -u ubuntu nohup sh -c "java -jar $JAR_PATH > $LOG_PATH" &
+echo "> $JAR_PATH 배포 >> sudo -u ubuntu nohup sh -c "nohup java -jar $JAR_NAME > $LOG_PATH 2>&1 &" &
+sudo -u ubuntu nohup sh -c "nohup java -jar $JAR_NAME > $LOG_PATH 2>&1 &" &
