df72a2cee299f444caaa311be16f95217e4fb8fc
Parkgeonmoo <pgw111111@naver.com>
2023-12-31 03:47:15 +0900

    Retry: 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,9 @@ APP_NAME=CatchRoom_BE
 JAR_NAME=$(ls $REPOSITORY/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/$JAR_NAME
 LOG_PATH=/home/ubuntu/log/catchroom.log
-SLACK_URL=$(aws secretsmanager get-secret-value --secret-id /secret/catchroom | jq -r '.SecretString' | jq -r | jq -r .slackurl)
 
-# test
-$(SLACK_URL > slackurl)
+# extract slack url from secrets manager
+SLACK_URL=$(aws secretsmanager get-secret-value --secret-id /secret/catchroom | jq -r '.SecretString' | jq -r | jq -r .slackurl)
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
@@ -32,4 +31,4 @@ fi
 
 echo "> $JAR_PATH 배포 " #>> sudo -u ubuntu nohup sh -c 'nohup java -jar $JAR_NAME > $LOG_PATH 2>&1 &' &"
 # sudo -u ubuntu nohup sh -c 'nohup java -jar $JAR_NAME > $LOG_PATH 2>&1 &' &
-sudo -u ubuntu nohup java -jar $JAR_PATH --slack.webhook.url=$SLACK_URL > /dev/null 2> /dev/null < /dev/null &
+sudo -u ubuntu nohup java -jar $JAR_PATH --slack.webhook.url=$SLACK_URL > $LOG_PATH 2> &1 &' &
