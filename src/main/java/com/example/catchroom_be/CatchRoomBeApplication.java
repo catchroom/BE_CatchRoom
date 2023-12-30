072e38347a5ba39d96ff69fd5e07e2e552ca2ecd
parkgeonwoo <50697545+Parkgeonmoo@users.noreply.github.com>
2023-12-30 21:54:12 +0900

    Refact : 수정

--- src/main/java/com/example/catchroom_be/CatchRoomBeApplication.java
+++ src/main/java/com/example/catchroom_be/CatchRoomBeApplication.java
@@ -1,6 +1,7 @@
 package com.example.catchroom_be;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,11 +10,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Slf4j
 @SpringBootApplication
+
 public class CatchRoomBeApplication {
+	@Value("{$log.slack.web-hook-url}")
+	private static String url;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatchRoomBeApplication.class, args);
 		log.error("캐치룸 서버 에러 모니터링 테스트");
+		log.error(url);
 	}
 
 }
