debda769358aac2a787ca831e43c47bf1ed794b9
parkgeonwoo <50697545+Parkgeonmoo@users.noreply.github.com>
2024-01-01 22:13:05 +0900

    Refact : 수정

--- src/main/java/com/example/catchroom_be/User/Controller/OAuthController.java
+++ src/main/java/com/example/catchroom_be/User/Controller/OAuthController.java
@@ -1,6 +1,5 @@
 package com.example.catchroom_be.User.Controller;
 
-import com.example.catchroom_be.User.Service.GoogleOAuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,11 +7,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 public class OAuthController {
-    private final GoogleOAuthService googleOAuthService;
 
     @PostMapping("/oauth2/callback")
-    public ResponseEntity<String> getAuthCode(@RequestParam String authCode) {
-        return googleOAuthService.getAccessToken(authCode);
+    public String getAuthCode(@RequestParam String authCode) {
+        return authCode;
     }
 }
 
