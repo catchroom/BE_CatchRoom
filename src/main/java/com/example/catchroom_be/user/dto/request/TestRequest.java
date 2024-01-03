efb386c744524a16256dc7c56743e3d83583383e
parkgeonwoo <50697545+Parkgeonmoo@users.noreply.github.com>
2024-01-04 00:48:52 +0900

    Refact : OAuth 테스트

---
+++ src/main/java/com/example/catchroom_be/user/dto/request/TestRequest.java
@@ -0,0 +1,14 @@
+package com.example.catchroom_be.user.dto.request;
+
+public class TestRequest {
+    String accessToken;
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+}
