ef217849c16a96eb2c0867b0b91084052d505b37
HyemIin <hyem5019@gmail.com>
2024-01-16 17:43:04 +0900

    fixed untracked files

---
+++ src/main/java/com/example/catchroom_be/domain/product/dto/response/SaleRegistResponse.java
@@ -0,0 +1,18 @@
+package com.example.catchroom_be.domain.product.dto.response;
+
+import com.example.catchroom_be.domain.product.entity.Product;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Builder
+public class SaleRegistResponse {
+    private String accommodationName;
+
+    public static SaleRegistResponse fromEntity(Product product) {
+        return SaleRegistResponse.builder()
+            .accommodationName(product.getAccommodationName())
+            .build();
+    }
+}
