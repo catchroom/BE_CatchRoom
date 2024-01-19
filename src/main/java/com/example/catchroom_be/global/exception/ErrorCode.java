8f964ee6c3eb82efe45f0fc6505f6131d472eccc
parkgeonwoo <50697545+Parkgeonmoo@users.noreply.github.com>
2024-01-19 18:35:09 +0900

    Feat : 예치금 상세 조회/출금 개발 완료

--- src/main/java/com/example/catchroom_be/global/exception/ErrorCode.java
+++ src/main/java/com/example/catchroom_be/global/exception/ErrorCode.java
@@ -26,8 +26,8 @@ public enum ErrorCode {
     MYPAGE_ACCOUNTNUM_REGISTER_REFACT_ERROR(2007,"예치금 계좌 등록/수정에 실패하였습니다."),
     MYPAGE_ACCOUNTNUM_DELETE_ERROR(2031,"예치금 계좌 삭제를 실패하였습니다."),
     MYPAGE_PROFILE_FIND_ERROR(2032,"회원 정보 조회에 실패하였습니다."),
-
-
+    MYPAGE_DEPOSIT_WITHDRAW_ERROR(2013,"예치금 잔액보다 출금 금액이 더 큽니다."),
+    MYPAGE_DEPOSIT_LIST_ERROR(2033,"에치금 상세 내역 보기가 실패하였습니다."),
     //예치금 통장 관련
     ACCOUNT_NOT_SAVED(1099, "계좌가 올바르게 저장되지 않았습니다."),
 
