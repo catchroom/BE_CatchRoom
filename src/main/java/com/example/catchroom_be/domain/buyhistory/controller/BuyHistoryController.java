420b6fd65f712e937431b69d4d3a7ebf552b3b16
YongHyunHong <dydgus1052@naver.com>
2024-01-20 16:20:45 +0900

    Feat: 구매 controller 추가

---
+++ src/main/java/com/example/catchroom_be/domain/buyhistory/controller/BuyHistoryController.java
@@ -0,0 +1,49 @@
+package com.example.catchroom_be.domain.buyhistory.controller;
+
+import com.example.catchroom_be.domain.buyhistory.dto.request.BuyRequest;
+import com.example.catchroom_be.domain.buyhistory.service.BuyHistoryService;
+import com.example.catchroom_be.domain.product.entity.Product;
+import com.example.catchroom_be.domain.product.repository.ProductRepository;
+import com.example.catchroom_be.domain.user.entity.User;
+import com.example.catchroom_be.global.common.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/v1/buy")
+@RequiredArgsConstructor
+public class BuyHistoryController {
+
+    private final BuyHistoryService buyHistoryService;
+    @PostMapping("")
+    public ApiResponse<?> buyProduct(
+            @AuthenticationPrincipal User user,
+            @RequestBody @Valid BuyRequest buyRequest
+    ) {
+
+        return buyHistoryService.buyProduct(user, buyRequest);
+    }
+
+    @GetMapping("/productInfo")
+    public ApiResponse<?> productInfo(
+            @AuthenticationPrincipal User user,
+            @RequestParam Long productId) {
+
+        System.out.println("productId = " + productId);
+
+        return buyHistoryService.getPaymentInfo(user, productId);
+    }
+
+    @GetMapping("/mypage/purchasehistory/detail")
+    public ApiResponse<?> getPurchaseHistory(
+            @AuthenticationPrincipal User user,
+            @RequestParam Long buyHistoryId
+    ) {
+
+        return buyHistoryService.purchaseHistory(user, buyHistoryId);
+    }
+}
