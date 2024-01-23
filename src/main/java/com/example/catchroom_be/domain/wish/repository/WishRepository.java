4a2ddcae3ebf959e87dd46f07891be738ea183c0
parkgeonwoo <50697545+Parkgeonmoo@users.noreply.github.com>
2024-01-23 18:10:25 +0900

    Feat : 찜 조회/삭제 개발

--- src/main/java/com/example/catchroom_be/domain/wish/repository/WishRepository.java
+++ src/main/java/com/example/catchroom_be/domain/wish/repository/WishRepository.java
@@ -5,7 +5,14 @@ import com.example.catchroom_be.domain.user.entity.User;
 import com.example.catchroom_be.domain.wish.entity.Wish;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface WishRepository extends JpaRepository<Wish, Long> {
 
     Wish findByUserAndProduct(User user, Product product);
+
+    List<Wish> findAllByUserId(Long userId);
+
+    Optional<Wish> findByIdAndUserId(Long id, Long userId);
 }
