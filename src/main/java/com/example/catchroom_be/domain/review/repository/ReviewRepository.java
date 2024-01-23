d8737aceb073b8356c4a289ce711a64b5e7f5969
YongHyunHong <dydgus1052@naver.com>
2024-01-23 16:20:47 +0900

    HotFix: Repository extends 추가

--- src/main/java/com/example/catchroom_be/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/example/catchroom_be/domain/review/repository/ReviewRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface ReviewRepository extends JpaRepository<Review,Long> {
+public interface ReviewRepository extends JpaRepository<Review,Long>, ReviewRepositoryCustom {
 
     Optional<Review> findByIdAndType(Long id, ReviewType type);
 
