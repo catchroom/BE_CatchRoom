9adac196523a14ceee5f55f93bc26f93d99dd606
YongHyunHong <dydgus1052@naver.com>
2024-01-23 16:05:50 +0900

    Feat: 리뷰 검색 Repository 추가

---
+++ src/main/java/com/example/catchroom_be/domain/review/repository/ReviewRepositoryImpl.java
@@ -0,0 +1,78 @@
+package com.example.catchroom_be.domain.review.repository;
+
+import com.example.catchroom_be.domain.product.dto.response.ProductSearchListResponse;
+import com.example.catchroom_be.domain.review.enumlist.ReviewSearchListResponse;
+import com.example.catchroom_be.domain.review.enumlist.ReviewSearchListResponse.ReviewSearchResponse;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.catchroom_be.domain.accommodation.entity.QAccommodation.accommodation;
+import static com.example.catchroom_be.domain.accommodation.entity.QRoom.room;
+import static com.example.catchroom_be.domain.orderhistory.entity.QOrderHistory.orderHistory;
+import static com.example.catchroom_be.domain.product.entity.QProduct.product;
+import static com.example.catchroom_be.domain.review.entity.QReview.review;
+import static com.example.catchroom_be.domain.user.entity.QUser.user;
+
+
+@RequiredArgsConstructor
+public class ReviewRepositoryImpl implements ReviewRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public ReviewSearchListResponse getReviewMain() {
+
+        List<ReviewSearchResponse> result =
+                queryFactory.selectFrom(review)
+                        .innerJoin(review.product,product).fetchJoin()
+                        .innerJoin(review.user,user).fetchJoin()
+                        .innerJoin(product.orderHistory,orderHistory).fetchJoin()
+                        .innerJoin(orderHistory.accommodation,accommodation).fetchJoin()
+                        .limit(10)
+                        .fetch()
+                        .stream()
+                        .map(ReviewSearchResponse::fromEntity)
+                        .toList();
+
+        return ReviewSearchListResponse.builder()
+                .size((long) result.size())
+                .list(result)
+                .nextPage(false)
+                .build();
+    }
+
+    @Override
+    public ReviewSearchListResponse getReviewAll(Pageable pageable) {
+
+        List<ReviewSearchResponse> result =
+                queryFactory.selectFrom(review)
+                        .innerJoin(review.product,product).fetchJoin()
+                        .innerJoin(review.user,user).fetchJoin()
+                        .innerJoin(product.orderHistory,orderHistory).fetchJoin()
+                        .innerJoin(orderHistory.accommodation,accommodation).fetchJoin()
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch()
+                .stream()
+                .map(ReviewSearchResponse::fromEntity)
+                .toList();
+
+        long totalSize = queryFactory.selectFrom(review)
+                .fetchCount();
+
+        int pageSize = pageable.getPageSize();
+        int pageNumber = pageable.getPageNumber();
+        boolean isNextPage = (pageNumber + 1) * pageSize < totalSize;
+
+        return ReviewSearchListResponse.builder()
+                .size(totalSize)
+                .nextPage(isNextPage)
+                .list(result)
+                .build();
+
+    }
+}
