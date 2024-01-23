c43d712e5deabc48dc9dac39b4a24a5dc0312b89
sungjiwoon <nebulaisme@naver.com>
2024-01-23 23:25:03 +0900

    refactor, style : 46번 채팅방 내부 정보 조회 api 코드 정리

--- src/main/java/com/example/catchroom_be/domain/chatroom/service/ChatRoomService.java
+++ src/main/java/com/example/catchroom_be/domain/chatroom/service/ChatRoomService.java
@@ -27,7 +27,6 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ChatRoomService {
     private final ProductRepository productRepository;
-
     private final ChatRoomRepository chatRoomRepository;
     private final UserEntityRepository userEntityRepository;
 
@@ -45,9 +44,11 @@ public class ChatRoomService {
 
     @Transactional(readOnly = true)
     public List<ChatRoomListGetResponse> findChatRoomListByMemberId(User user) {
+
         List<ChatRoom> ChatRoomListUserIsBuyer = chatRoomRepository.findAllByBuyerIdOrSellerId(
                 user.getId(),user.getId()
         );
+
         List<ChatRoom> chatRooms = new ArrayList<>();
 
         for (ChatRoom chatRoom : ChatRoomListUserIsBuyer) {
@@ -59,30 +60,10 @@ public class ChatRoomService {
             chatRoom.updateUserIdentity(user.getId());
             chatRooms.add(chatRoom);
         }
+
         return chatRooms.stream()
             .map(ChatRoomListGetResponse::fromEntity)
             .collect(Collectors.toList());
-    }
-
-    private String getPartnerNickName(User user, ChatRoom chatRoom) {
-        String partnerNickName = "";
-        User partner = null;
-
-        if (user.getId() == chatRoom.getBuyer().getId()) {
-            partner = userEntityRepository.findById(chatRoom.getSeller().getId())
-                    .orElseThrow(
-                            () -> new UserException(ErrorCode.CHATROOM_PARTNER_USER_NOT_FOUND));
-        } else if (user.getId() == chatRoom.getSeller().getId()){
-            partner = userEntityRepository.findById(chatRoom.getBuyer().getId())
-                    .orElseThrow(
-                            () -> new UserException(ErrorCode.CHATROOM_PARTNER_USER_NOT_FOUND));
-        } else {
-            throw new ChatRoomException(ErrorCode.CHATROOM_USER_NOT_FOUND);
-        }
-
-        partnerNickName = partner.getNickName();
-
-        return partnerNickName;
     }
 
     @Transactional
@@ -90,20 +71,23 @@ public class ChatRoomService {
         ChatRoom chatRoom = chatRoomRepository.findByChatRoomNumber(roomId)
                 .orElseThrow(() -> new ChatRoomException(ErrorCode.CHATROOM_NOT_FOUND));
 
-        StringBuilder sb = new StringBuilder();
+        StringBuilder logSb = new StringBuilder();
 
         if (chatRoom.getSeller().getId().equals(user.getId())) {
             chatRoom.updateSellerState(ChatRoomState.DONT_SEE);
-            sb.append("this is seller -> ");
+            logSb.append("this is seller -> ");
         } else if (chatRoom.getBuyer().getId().equals(user.getId())) {
             chatRoom.updateBuyerState(ChatRoomState.DONT_SEE);
-            sb.append("this is buyer -> ");
+            logSb.append("this is buyer -> ");
         }
 
         ChatRoom saveChatRoom = chatRoomRepository.save(chatRoom);
 
-        sb.append(roomId).append(" : ").append(user.getId()).append(" delete SUCCESS");
-        if (saveChatRoom == null) return SuccessMessage.createSuccessMessage("FAILED");
-        return SuccessMessage.createSuccessMessage(sb.toString());
+        logSb.append(roomId + " : " + user.getId() + " delete");
+
+        if (saveChatRoom == null)
+            return SuccessMessage.createSuccessMessage(logSb.toString() + " FAILED");
+
+        return SuccessMessage.createSuccessMessage(logSb.toString() + " SUCCESS");
     }
 }
